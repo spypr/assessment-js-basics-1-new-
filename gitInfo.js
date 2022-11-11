2f229753562bf6df21afc6344c9891adc1239d94
Steven Pyper <steven.pyper@gmail.com>
2022-11-11 13:43:46 -0700

    Initial commit

---
+++ gitInfo.js
@@ -0,0 +1,79 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitDefinition = (`A popular free and open source software program that is used for collaboartion to program source code during development.`)
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDeifinition = (`Cloud based hosting service of git code projects where users can efficiently collaborate, track versions, and review edits.`)
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInit = (`creates a new repository in the current folder`)
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = (`Creates a copy of a repository and tells it where to put the copy of repository`)
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = (`Lists what staged changes have been made to a repository and what is in the staging area`)
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = (`Allows you to add files to be tracked in the staging area`)
+let gitAddCode = (`git add`)
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+let gitCommitDefinition = (`Allows you to create a snapshot of your repository.  It creates unique ID in the timeline when used.`)
+let gitCommitCode = (`git commit`)
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = (`Allows you to upload or transfer your local repository to remote repository like GitHub`)
---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const AnalysisOfRequirements = "This is the second step, this step looks at what the users will need to be able to do in the application such as being able to upload photos on a social media project or being able to have a search bar to look for friends in your friends list."
+const Design = "This is the third step, this step looks at what programming languages will be used, how the interface interacts with the users, security measures taken, and how it will communicate with other applications"
+const Implementation = "This is the fourth step, and where the coding actually starts to begin.  Individuals or teams put the code together and document the process"
+const TestingAndIntegration = "This is the fifth step, testing is done to understand the security risks and abilities, the speed of the application, and general performance issues.  It is then integrated with other programs and APIs"
+const Maintain = "This is the sixth step, the application has been tested and implemented but it needs to be maintained.  Bugs will be discovered by users and errors need to be quickly resolved."
