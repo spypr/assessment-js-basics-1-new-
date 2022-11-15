1da67936c0505efa0e691cb2c17f2cfe3b1ac8b1
Steven Pyper <steven.pyper@gmail.com>
2022-11-14 22:05:20 -0700

    updates and additions - Steve

--- sdlc.js
+++ sdlc.js
@@ -19,8 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const AnalysisOfRequirements = "This is the second step, this step looks at what the users will need to be able to do in the application such as being able to upload photos on a social media project or being able to have a search bar to look for friends in your friends list."
-const Design = "This is the third step, this step looks at what programming languages will be used, how the interface interacts with the users, security measures taken, and how it will communicate with other applications"
+const AnalysisOfRequirements = "This is the second step, this step looks at what the systems will need to be used to make the project run and what user applications will need to be added"
+const Design = "This is the third step,  how the interface interacts with the users, security measures taken, and how it will communicate with other applications"
 const Implementation = "This is the fourth step, and where the coding actually starts to begin.  Individuals or teams put the code together and document the process"
 const TestingAndIntegration = "This is the fifth step, testing is done to understand the security risks and abilities, the speed of the application, and general performance issues.  It is then integrated with other programs and APIs"
-const Maintain = "This is the sixth step, the application has been tested and implemented but it needs to be maintained.  Bugs will be discovered by users and errors need to be quickly resolved."
+const Maintain = "This is the sixth step, the application has been tested and implemented but it needs to be maintained.  Bugs will be discovered by users and errors need to be quickly resolved.  Further design will be updated"
